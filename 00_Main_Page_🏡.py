1f0cbf80f1482be8980995efb06e57410339d1db
ysntrkc <yasin.trkc.142@gmail.com>
2022-08-27 16:18:45 +0300

    finish streamlit page and update requirements

--- 00_Main_Page_🏡.py
+++ 00_Main_Page_🏡.py
@@ -1,9 +1,7 @@
-import numpy as np
 import streamlit as st
 import tensorflow as tf
 from keras.models import load_model
-from keras.preprocessing.image import ImageDataGenerator
-from keras.utils import array_to_img, img_to_array, load_img
+from keras.utils import img_to_array
 from PIL import Image
 
 IMAGE_DIM = (150, 150)
@@ -11,12 +9,13 @@ IMAGE_DIM = (150, 150)
 st.set_page_config(page_title="🏡 Main Page", layout="centered")
 st.title("Welcome to Cat-Dog Classifier")
 
-st.markdown("""
-    ### Upload an image of cat or dog and see the prediction!
-""")
+st.markdown("---")
+st.subheader("Upload an image of cat or dog and see the prediction!")
 
-uploaded_file = st.file_uploader("Upload an image...", type=["jpg", "png"])
-model = load_model('model/vgg_transfer_learn_dogvscat_2.h5')
+uploaded_file = st.file_uploader(label="", type=["jpg", "png"])
+st.markdown("---")
+
+model = load_model('model/vgg_transfer_learn_dogvscat.h5')
 
 def resize_image(img):
     img = tf.image.resize(img, IMAGE_DIM)
@@ -27,7 +26,7 @@ def resize_image(img):
 def predict(img):
     img_arr = resize_image(img)
     img_arr = img_arr.reshape((1,) + img_arr.shape)
-    img_arr = img_arr.astype('float32')
+    img_arr = img_arr.astype('float16')
     img_arr /= 255.0
 
     pred = model.predict(img_arr)
@@ -37,9 +36,9 @@ if uploaded_file is not None:
     img = Image.open(uploaded_file)
     pred = predict(img)
 
-    st.image(img, use_column_width=True)
-
-    if pred[0][0] > 0.6:
-        st.write("This is a Dog!")
+    if pred[0][0] > 0.55:
+        st.subheader("Predicted: Dog!")
     else:
-        st.write("This is a Cat!")
+        st.subheader("Predicted: Cat!")
+
+    st.image(img, use_column_width=True)
